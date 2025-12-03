cb0b0ec6a8d67a4b2aeaa61751585b668a94769f
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2025-12-03 04:37:46 +0300

    Fix OOB

--- Assets/BEANScript.cs
+++ Assets/BEANScript.cs
@@ -105,7 +105,6 @@ public class BEANScript : MonoBehaviour
                     StartCoroutine(PlaySoundsSequentially());
                 }
             }
-            Destroy(gameObject);
         }
 
     }
