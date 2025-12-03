b557dda4058ac870df85bdd7eacc3c49f6617854
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2025-12-03 10:00:50 +0300

    BeanImage Update

--- Assets/StartScript.cs
+++ Assets/StartScript.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class StartScript : MonoBehaviour
@@ -6,6 +7,7 @@ public class StartScript : MonoBehaviour
     public string sceneToLoad = "Game";
     public TMP_Text highScoreText;
     public TMP_Dropdown difficultyDropdown;
+    public Image beanImage;
     public ShopUI shopUI;
     private int previousDifficulty = 0;
     void Update()
@@ -30,6 +32,35 @@ public class StartScript : MonoBehaviour
             difficultyDropdown.value = previousDifficulty;
         }
     }
+
+    void Start()
+    {
+        UpdateBeanImage();
+    }
+
+    public void UpdateBeanImage()
+    {
+        if (beanImage == null) return;
+
+        if (ShopManager.Instance != null)
+        {
+            CharacterData selectedChar = ShopManager.Instance.GetSelectedCharacter();
+            if (selectedChar != null)
+            {
+                if (selectedChar.overrideSprite != null)
+                {
+                    beanImage.sprite = selectedChar.overrideSprite;
+                    beanImage.color = Color.white;
+                }
+                else
+                {
+                    // Assuming the default sprite is already set or we don't want to change it if no override
+                    // But we should probably set color if it's a tint-based character
+                    beanImage.color = selectedChar.color;
+                }
+            }
+        }
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(sceneToLoad);
@@ -41,6 +72,7 @@ public class StartScript : MonoBehaviour
     public void CloseShop()
     {
         if (shopUI != null) shopUI.CloseShop();
+        UpdateBeanImage();
     }
     public void QuitGame()
     {
